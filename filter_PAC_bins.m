38d92994d7092b68a0217c0ab88b37d930690223
Zach <ipzach@uw.edu>
2022-09-20 10:39:06 -0700

    phase amplitude coupling functions

--- filter_PAC_bins.m
+++ filter_PAC_bins.m
@@ -3,8 +3,15 @@ function [FreqTransform] = filter_PAC_bins(signal, FreqVector, bandwidth, Fs)
 Generate matrix of eeg filtered signals.
 
 Args:
-    
+    signal (1xn array): signal to be filter
+    FreqVector (1xn array): Array specifying how many and which frequencies 
+    to filter
+    bandwidth (int): how wide of a signal window to filter
+    Fs (int): sampling frequency
 
+Returns:
+    FreqTransform (m x n matrix): where m is where m is number of frequencies, and n is signal
+    length, and each row is a filtered version of the eeg. 
 %}
 
 
--- isolate_state_idx.m
+++ isolate_state_idx.m
@@ -4,9 +4,12 @@ Generate an array of indices for data points according to an array of start
 and end times, recording frequency
 
 Args:
-    starts (array): List of start times in seconds
-    ends (array): List of end times in seconds
+    starts (1xn array): List of start times in seconds
+    ends (1xn array): List of end times in seconds
     Fs (int): Recording frequency
+
+Returns:
+    idx (1xn boolean): mask of when state is active during recording
 %}
 
  idx  = [];
