301bc2f5d847369ac195e461ccc0cef16d25956c
Zach <ipzach@uw.edu>
2022-11-22 21:43:13 -0800

    added multi user support to excel save functions

--- UCSF_create_excel.m
+++ UCSF_create_excel.m
@@ -1,8 +1,12 @@
-function UCSF_create_excel(measure, vals,file_name)
+function UCSF_create_excel(measure, vals,file_name, user)
 
 formatted_vals  = [vals.means; vals.SD; vals.n];
 Datetime = string(datetime('now'));
-cd(['C:\COM\ePhy\dbdb\Data\Outputs\Data\' measure])
+if strcmp(user,'S')
+    cd(['C:\COM\ePhy\dbdb\Data\Outputs\Data\' measure])
+else
+    cd('C:\Users\ipzach\Documents\MATLAB\output\excel');
+end
 Filename = sprintf([file_name '_data_%s.xlsx'], Datetime);
 Filename = regexprep(Filename, {' ', ':', '-'}, {'_', '_', '_'});
 xlswrite(Filename, formatted_vals);
